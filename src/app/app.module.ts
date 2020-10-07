0347bbb7c56fae795ed220ca980e28d3c6484551
Suren <suren.boyilla@gmail.com>
2020-10-07 10:18:41 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateMemberComponent } from './member/create-member/create-member.component';
+import { UpdateMemberComponent } from './member/update-member/update-member.component';
+import { MemberListComponent } from './member/member-list/member-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateMemberComponent,
+    UpdateMemberComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule,
